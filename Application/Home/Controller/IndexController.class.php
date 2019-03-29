ed9403df7caea7175abaa2594c4288cf68eff211
wyx <1822581649@qq.com>
2019-03-29 13:19:09 +0800

    style:优化代码格式

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,33 +1,52 @@
 <?php
+/**
+ * TP3-CMS
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   ThinkPHP3.2
+ * @author    wangyaxian <1822581649@qq.com>
+ * @link      https://github.com/duiying/TP3-CMS
+ */
+
 namespace Home\Controller;
 use Think\Controller;
 
 /**
- * 首页
+ * 首页控制器
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   ThinkPHP3.2
+ * @author    wangyaxian <1822581649@qq.com>
+ * @link      https://github.com/duiying/TP3-CMS
  */
-
 class IndexController extends Controller 
 {
 	/**
 	 * 首页
 	 */
-	public function index() {
+	public function index()
+    {
 		// 直接传入模板文件名
-		$this->display('./index.html');
+		$this->display('index.html');
 	}
 
 
     /**
      * 新闻列表
      */
-    public function news() {
-    	//公共部分
+    public function news()
+    {
+    	// 公共部分
 		$res = D('Common')->common();
-		foreach($res as $key=>$value) {
+		foreach ($res as $key=>$value) {
 			$this->assign($key, $value);
 		}
 
-		$list = D('Common')->datalist('Article', array(), '*', 'art_sort, art_id');			
+		$list = D('Common')->datalist('Article', [], '*', 'art_sort, art_id');
 
 		$this->assign('list', $list['list']);
 		$this->assign('page', $list['page']);
@@ -35,5 +54,4 @@ class IndexController extends Controller
 
 		$this->display();
     }
-
 }
